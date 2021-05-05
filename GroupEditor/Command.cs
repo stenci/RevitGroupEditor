89a5ead5f70b49d3e70869e0d69fa7512dd2edfa
Stefano Menci <stefano.menci@gmail.com>
2021-05-04 20:26:42 -0500

    First commit

---
+++ GroupEditor/Command.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace GroupEditor
+{
+    [Transaction(TransactionMode.Manual)]
+    public class GroupEditorStartEditing : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var app = commandData.Application;
+            var uidoc = app.ActiveUIDocument;
+            var doc = uidoc.Document;
+
+            var selectedElements = uidoc.Selection.GetElementIds();
+            if (selectedElements.Count != 1)
+            {
+                TaskDialog.Show("Group Editor",
+                    "Please select a group and try again.");
+                return Result.Succeeded;
+            }
+
+            var group = doc.GetElement(selectedElements.First()) as Group;
+            if (group == null)
+            {
+                TaskDialog.Show("Group Editor",
+                    "Please select a group and try again.");
+                return Result.Succeeded;
+            }
+
+            using (var tx = new Transaction(doc))
+            {
+                tx.Start("Group Editor Start Editing");
+
+                var groupEditor = new GroupEditor(group);
+                groupEditor.StartEditing();
+
+                tx.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+
+    [Transaction(TransactionMode.Manual)]
+    public class GroupEditorFinishEditing : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var app = commandData.Application;
+            var uidoc = app.ActiveUIDocument;
+            var doc = uidoc.Document;
+
+            var groupName = Utils.PickOneOfTheGroupsBeingEdited(doc,
+                "Select the group to finish the editing.",
+                "Group {} has been restored.");
+
+            if (groupName == null)
+                return Result.Cancelled;
+
+            using (var tx = new Transaction(doc))
+            {
+                tx.Start("Group Editor Finish Editing");
+
+                var groupEditor = new GroupEditor(doc, groupName);
+                groupEditor.FinishEditing();
+
+                tx.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+
+    [Transaction(TransactionMode.Manual)]
+    public class GroupEditorAddToGroup : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var app = commandData.Application;
+            var uidoc = app.ActiveUIDocument;
+            var doc = uidoc.Document;
+
+            var selectedElements = uidoc.Selection.GetElementIds();
+            if (selectedElements.Count == 0)
+            {
+                TaskDialog.Show("Group Editor",
+                    "Please select the elements to add to the group and try again.");
+                return Result.Succeeded;
+            }
+
+            var groupName = Utils.PickOneOfTheGroupsBeingEdited(doc,
+                "Select the group you want to add the pre-selected elements to.",
+                "The pre-selected elements have been added to group {}.");
+
+            if (groupName == null)
+                return Result.Cancelled;
+
+            using (var tx = new Transaction(doc))
+            {
+                tx.Start("Group Editor Add To Group");
+
+                var groupEditor = new GroupEditor(doc, groupName);
+
+                foreach (var elementId in selectedElements)
+                    groupEditor.AddElement(doc.GetElement(elementId));
+
+                tx.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+
+    [Transaction(TransactionMode.Manual)]
+    public class GroupEditorPurge : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var app = commandData.Application;
+            var uidoc = app.ActiveUIDocument;
+            var doc = uidoc.Document;
+
+            var groupName = Utils.PickOneOfTheGroupsBeingEdited(doc,
+                "Select the group whose entities you want to delete the group being edited info.",
+                "Group {} has been deleted.");
+
+            if (groupName == null)
+                return Result.Cancelled;
+
+            using (var tx = new Transaction(doc))
+            {
+                tx.Start("Group Editor Purge");
+
+                var groupEditor = new GroupEditor(doc, groupName);
+                groupEditor.DeleteAllGroupEditorSchemas();
+
+                tx.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+
+    static class Utils
+    {
+        public static string PickOneOfTheGroupsBeingEdited(Document doc,
+            string mainInstruction, string messageIfOnlyOneName)
+        {
+            var groupNames = GroupEditor.GetNamesOfGroupsBeingEdited(doc).ToList();
+            groupNames.Sort();
+
+            if (groupNames.Count == 0)
+            {
+                TaskDialog.Show("Group Editor",
+                    "There are no groups being edited.");
+                return null;
+            }
+
+            if (groupNames.Count == 1)
+            {
+                if (messageIfOnlyOneName != null)
+                    TaskDialog.Show("Group Editor",
+                        messageIfOnlyOneName.Replace("{}", groupNames[0]));
+                return groupNames[0];
+            }
+
+            var dialog = new TaskDialog("Group Editor") {MainInstruction = mainInstruction};
+
+            if (groupNames.Count > 4)
+                dialog.MainContent = $"Only 4 of the {groupNames.Count} groups being edited are listed.";
+
+            dialog.AddCommandLink(TaskDialogCommandLinkId.CommandLink1, groupNames[0]);
+            dialog.AddCommandLink(TaskDialogCommandLinkId.CommandLink2, groupNames[1]);
+            if (groupNames.Count >= 3) dialog.AddCommandLink(TaskDialogCommandLinkId.CommandLink3, groupNames[2]);
+            if (groupNames.Count >= 4) dialog.AddCommandLink(TaskDialogCommandLinkId.CommandLink4, groupNames[3]);
+
+            var result = dialog.Show();
+
+            switch (result)
+            {
+                case TaskDialogResult.Cancel: return null;
+                case TaskDialogResult.CommandLink1: return groupNames[0];
+                case TaskDialogResult.CommandLink2: return groupNames[1];
+                case TaskDialogResult.CommandLink3: return groupNames[2];
+                case TaskDialogResult.CommandLink4: return groupNames[3];
+                default: throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ GroupEditor/GroupEditor.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.ExtensibleStorage;
+using InvalidOperationException = System.InvalidOperationException;
+
+namespace GroupEditor
+{
+    class GroupEditor
+    {
+        private Group _group;
+        private readonly Document _doc;
+
+        private readonly string _groupName;
+        private readonly bool _pinned;
+        private readonly XYZ _locationPoint;
+
+        private static Schema _schema;
+        private static Field _fieldGroupName, _fieldPinned, _fieldLocationPoint;
+
+        public GroupEditor(Group group)
+        {
+            GetSchema();
+
+            _doc = group.Document;
+            _group = group;
+
+            _groupName = group.GroupType.Name;
+            _pinned = group.Pinned;
+            _locationPoint = (group.Location as LocationPoint).Point;
+        }
+
+        public GroupEditor(Document doc, string ungroupedGroupName)
+        {
+            GetSchema();
+
+            _doc = doc;
+            _group = null;
+            _groupName = ungroupedGroupName;
+            var firstElementSchema = GroupElements().First().GetEntity(_schema);
+            _pinned = firstElementSchema.Get<bool>("Pinned");
+            _locationPoint = firstElementSchema.Get<XYZ>("LocationPoint", UnitTypeId.Feet);
+        }
+
+        public void StartEditing()
+        {
+            if (_group == null)
+                throw new InvalidOperationException("The group is already open");
+
+            foreach (var id in _group.UngroupMembers())
+                SetElementSchema(_doc.GetElement(id));
+
+            _group = null;
+        }
+
+        public void AddElement(Element element)
+        {
+            SetElementSchema(element);
+        }
+
+        private void SetElementSchema(Element element)
+        {
+            var schemaEntity = new Entity(_schema);
+            schemaEntity.Set(_fieldGroupName, _groupName);
+            schemaEntity.Set(_fieldPinned, _pinned);
+            schemaEntity.Set(_fieldLocationPoint, _locationPoint, UnitTypeId.Feet);
+            element.SetEntity(schemaEntity);
+        }
+
+        public void FinishEditing()
+        {
+            var elements = new List<ElementId>();
+            foreach (var element in GroupElements())
+            {
+                elements.Add(element.Id);
+                element.DeleteEntity(_schema);
+            }
+
+            _group = _doc.Create.NewGroup(elements);
+
+            var oldGroupType = new FilteredElementCollector(_doc)
+                .OfClass(typeof(GroupType))
+                .Cast<GroupType>()
+                .FirstOrDefault(group => group.Name == _groupName);
+
+            if (oldGroupType != null)
+            {
+                foreach (Group group in oldGroupType.Groups)
+                {
+                    group.GroupType = _group.GroupType;
+                    group.Location.Move((_group.Location as LocationPoint).Point - _locationPoint);
+                }
+
+                _doc.Delete(oldGroupType.Id);
+            }
+
+            _group.GroupType.Name = _groupName;
+            _group.Pinned = _pinned;
+        }
+
+        public static IEnumerable<string> GetNamesOfGroupsBeingEdited(Document doc)
+        {
+            GetSchema();
+
+            return new FilteredElementCollector(doc)
+                .WherePasses(new ExtensibleStorageFilter(_schema.GUID))
+                .Select(element => element.GetEntity(_schema).Get<string>("GroupName"))
+                .Distinct();
+        }
+
+        public void DeleteAllGroupEditorSchemas()
+        {
+            foreach (var element in GroupElements())
+                element.DeleteEntity(_schema);
+        }
+
+        public IEnumerable<Element> GroupElements()
+        {
+            return new FilteredElementCollector(_doc)
+                .WherePasses(new ExtensibleStorageFilter(_schema.GUID))
+                .Where(element => element.GetEntity(_schema).Get<string>("GroupName") == _groupName);
+        }
+
+        private static void GetSchema()
+        {
+            if (_schema != null) goto getSchemaFields;
+
+            var schemaGuid = new Guid("98140745-875d-4e4e-8e5e-a36146a4e844");
+            _schema = Schema.Lookup(schemaGuid);
+            if (_schema != null) goto getSchemaFields;
+
+            var schemaBuilder = new SchemaBuilder(schemaGuid);
+            schemaBuilder.SetSchemaName("GroupEditor");
+            schemaBuilder.SetReadAccessLevel(AccessLevel.Public);
+            schemaBuilder.SetWriteAccessLevel(AccessLevel.Public);
+            schemaBuilder.AddSimpleField("GroupName", typeof(string));
+            schemaBuilder.AddSimpleField("Pinned", typeof(bool));
+            schemaBuilder.AddSimpleField("LocationPoint", typeof(XYZ)).SetSpec(SpecTypeId.Length);
+            _schema = schemaBuilder.Finish();
+
+            getSchemaFields:
+            if (_fieldGroupName != null) return;
+            _fieldGroupName = _schema.GetField("GroupName");
+            _fieldPinned = _schema.GetField("Pinned");
+            _fieldLocationPoint = _schema.GetField("LocationPoint");
+        }
+    }
+}
